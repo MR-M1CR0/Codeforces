cb643b8e4b16ead145db88970c989c58c71b0504
Ahmed Mahfouz <ahmed.ahmed240@edu.menofia.edu.eg>
2024-01-26 00:02:27 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 int main()
 {
-    string userInput = "";
-    cin >> userInput;
-    cout << "Hello, " << userInput << endl;
+    int num = 0;
+    long long bigNum = 0;
+    char letter = ' ';
+    float decimal = 0;
+    double decimalNum = 0;
+
+    cin >> num  >> bigNum >> letter >> decimal >> decimalNum;
+    cout << num << endl << bigNum << endl << letter << endl << decimal << endl << decimalNum << endl;
     return 0;
 }
