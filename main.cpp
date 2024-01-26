b0d5a218ef8845d001040f0b653103ebf66fd431
Ahmed Mahfouz <ahmed.ahmed240@edu.menofia.edu.eg>
2024-01-26 10:31:18 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long firstNum = 0, secondNum = 0;
+    cin >> firstNum >> secondNum;
+    cout << firstNum << " + " << secondNum << " = " << firstNum + secondNum << endl;
+    cout << firstNum << " * " << secondNum << " = " << firstNum * secondNum << endl;
+    cout << firstNum << " - " << secondNum << " = " << firstNum - secondNum << endl;
+    return 0;
+}
