b6e3fd8e4cbcf086f060c0ad672d6ef56f61aca3
Ahmed Mahfouz <ahmed.ahmed240@edu.menofia.edu.eg>
2024-01-26 14:42:26 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double A = 0, B = 0;
+    int round, temp;
+    cin >> A >> B;
+    int floor = A / B;
+    int ceil = A != B ? floor + 1 : floor;
+    temp = (A / B) * 10;
+    if(temp % 10 < 5)
+        round = floor;
+    else
+        round = ceil;
+    cout << "floor " << A << " / " << B << " = " << floor << endl;
+    cout << "ceil " << A << " / " << B << " = " << ceil << endl;
+    cout << "round " << A << " / " << B << " = " << round << endl;
+    return 0;
+}
