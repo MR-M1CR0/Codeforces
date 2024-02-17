8b841d8b489d4f5112104ca4220c49d4bb31a03f
Ahmed Mahfouz <ahmed.ahmed240@edu.menofia.edu.eg>
2024-02-17 14:37:17 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if(N >= 2)
+    {
+        for(int i = 2; i <= N; i += 2)
+            cout << i << endl;
+    }
+    else
+        cout << -1 << endl;
+
+   return 0;
+}
